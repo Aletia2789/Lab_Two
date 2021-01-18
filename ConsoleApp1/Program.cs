ac4612f3659da959cf7b569126de2c53a3c47cee
Aletia <a.holloway2789@gmail.com>
2021-01-17 23:40:25 -0500

    First Commit for Lab Two

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace Lab_Two
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string answer = "y";
+            Console.WriteLine("Welcome to The Room Calculator!");
+            do
+            {
+                Console.Write("Please enter the width of your room: ");
+                var WidthInput = Console.ReadLine();
+                var InputW = double.Parse(WidthInput);
+
+                Console.Write("Please enter the length of your room: ");
+                var LengthInput = Console.ReadLine();
+                var InputL = double.Parse(LengthInput);
+
+                var ResultA = InputL * InputW;
+                var ResultP = 2 * InputL + 2 * InputW;
+                Console.WriteLine("The area of your room is " + ResultA + " and the perimeter of your room is " + ResultP + "\nWould you like to continue (y/n)?");
+                answer = Console.ReadLine();
+            } while (answer=="y");
+            Console.WriteLine("Thank you for using The Room Calculator! Goodbye.");
+                
+
+
+        
         }
     }
 }
